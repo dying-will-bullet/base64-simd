55196aa3e0c773041c3f3ea3e4932ec9b688e5c2
Hanaasagi <ambiguous404@gmail.com>
2023-07-04 15:15:14 +0900

    chore: refactor build.zig

--- build.zig
+++ build.zig
@@ -1,8 +1,31 @@
 const std = @import("std");
 const builtin = @import("builtin");
+const Build = std.Build;
+const CompileStep = std.build.CompileStep;
 
-pub fn generateConfig() !void {
-    var file = try std.fs.cwd().createFile("deps/base64/lib/config.h", .{});
+// C Source
+const source_files = &.{
+    "deps/base64/lib/arch/avx512/codec.c",
+    "deps/base64/lib/arch/avx2/codec.c",
+    "deps/base64/lib/arch/generic/codec.c",
+    "deps/base64/lib/arch/neon32/codec.c",
+    "deps/base64/lib/arch/neon64/codec.c",
+    "deps/base64/lib/arch/ssse3/codec.c",
+    "deps/base64/lib/arch/sse41/codec.c",
+    "deps/base64/lib/arch/sse42/codec.c",
+    "deps/base64/lib/arch/avx/codec.c",
+    "deps/base64/lib/lib.c",
+    "deps/base64/lib/codec_choose.c",
+    "deps/base64/lib/tables/tables.c",
+};
+
+fn dir() []const u8 {
+    return std.fs.path.dirname(@src().file) orelse ".";
+}
+
+pub fn generateConfig(b: *Build) !void {
+    const path = try std.fs.path.join(b.allocator, &.{ dir(), "deps/base64/lib/config.h" });
+    var file = try std.fs.cwd().createFile(path, .{});
     defer file.close();
 
     // var buf_writer = std.io.BufferedWriter(64, file.writer());
@@ -54,12 +77,48 @@ pub fn generateConfig() !void {
     }
 }
 
+fn buildLibBase64(b: *Build, step: *CompileStep) !*CompileStep {
+    // Reference:
+    // 1. https://github.com/kristoff-it/redis/blob/zig/build.zig
+    // 2. https://github.com/natecraddock/ziglua/blob/main/build.zig
+    const lib64 = b.addStaticLibrary(.{
+        .name = "libbase64",
+        .target = step.target,
+        .optimize = step.optimize,
+    });
+    // For `__stack_chk_fail`
+    lib64.linkLibC();
+    // C Source
+    inline for (source_files) |file| {
+        lib64.addCSourceFile(
+            try std.fs.path.join(b.allocator, &.{ dir(), file }),
+            &.{
+                "-std=c99",
+                "-O3",
+                "-Wall",
+                "-Wextra",
+                "-pedantic",
+            },
+        );
+    }
+    // config.h
+    lib64.addIncludePath(try std.fs.path.join(b.allocator, &.{ dir(), "deps/base64/lib" }));
+    // header
+    step.addIncludePath(try std.fs.path.join(b.allocator, &.{ dir(), "deps/base64/include" }));
+
+    return lib64;
+}
+
+pub fn buildAndLink(b: *Build, step: *CompileStep) !void {
+    try generateConfig(b);
+    const lib64 = try buildLibBase64(b, step);
+    step.linkLibrary(lib64);
+}
+
 // Although this function looks imperative, note that its job is to
 // declaratively construct a build graph that will be executed by an external
 // runner.
 pub fn build(b: *std.Build) void {
-    generateConfig() catch @panic("generate config failed");
-
     // Standard target options allows the person running `zig build` to choose
     // what target to build for. Here we do not override the defaults, which
     // means any target is allowed, and the default is native. Other options
@@ -71,35 +130,6 @@ pub fn build(b: *std.Build) void {
     // set a preferred release mode, allowing the user to decide how to optimize.
     const optimize = b.standardOptimizeOption(.{});
 
-    // Reference: https://github.com/kristoff-it/redis/blob/zig/build.zig
-    const lib64 = b.addStaticLibrary(.{
-        .name = "libbase64",
-        .target = target,
-        .optimize = optimize,
-    });
-    lib64.linkLibC();
-    lib64.addCSourceFiles(&.{
-        "deps/base64/lib/arch/avx512/codec.c",
-        "deps/base64/lib/arch/avx2/codec.c",
-        "deps/base64/lib/arch/generic/codec.c",
-        "deps/base64/lib/arch/neon32/codec.c",
-        "deps/base64/lib/arch/neon64/codec.c",
-        "deps/base64/lib/arch/ssse3/codec.c",
-        "deps/base64/lib/arch/sse41/codec.c",
-        "deps/base64/lib/arch/sse42/codec.c",
-        "deps/base64/lib/arch/avx/codec.c",
-        "deps/base64/lib/lib.c",
-        "deps/base64/lib/codec_choose.c",
-        "deps/base64/lib/tables/tables.c",
-    }, &.{
-        "-std=c99",
-        "-O3",
-        "-Wall",
-        "-Wextra",
-        "-pedantic",
-    });
-    lib64.addIncludePath("deps/base64/lib");
-
     const lib = b.addStaticLibrary(.{
         .name = "base64",
         // In this case the main source file is merely a path, however, in more
@@ -108,9 +138,8 @@ pub fn build(b: *std.Build) void {
         .target = target,
         .optimize = optimize,
     });
-    lib.linkLibC();
-    lib.addIncludePath("deps/base64/include");
-    lib.linkLibrary(lib64);
+
+    buildAndLink(b, lib) catch @panic("build failed");
 
     // This declares intent for the library to be installed into the standard
     // location when the user invokes the "install" step (the default step when
@@ -124,9 +153,7 @@ pub fn build(b: *std.Build) void {
         .target = target,
         .optimize = optimize,
     });
-    main_tests.linkLibC();
-    main_tests.addIncludePath("deps/base64/include");
-    main_tests.linkLibrary(lib64);
+    buildAndLink(b, main_tests) catch @panic("build failed");
 
     const run_main_tests = b.addRunArtifact(main_tests);
 
@@ -148,9 +175,7 @@ pub fn build(b: *std.Build) void {
         .source_file = .{ .path = "src/lib.zig" },
     });
     bench_exe.addModule("base64", mod);
-    bench_exe.linkLibC();
-    bench_exe.addIncludePath("deps/base64/include");
-    bench_exe.linkLibrary(lib64);
+    buildAndLink(b, bench_exe) catch @panic("build failed");
 
     b.installArtifact(bench_exe);
     const run_cmd = b.addRunArtifact(bench_exe);
