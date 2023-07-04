230434c0608787122e45033395cba989beff0b6f
Hanaasagi <ambiguous404@gmail.com>
2023-07-04 11:36:49 +0900

    feat: streaming api

--- src/lib.zig
+++ src/lib.zig
@@ -22,27 +22,112 @@ pub const Flag = enum(c_int) {
     avx512 = 1 << 8,
 };
 
-// pub const LibBase64 = struct {
-//     pub const State = extern struct {
-//         eof: c_int,
-//         bytes: c_int,
-//         flags: c_int,
-//         carry: u8,
-//     };
-//     pub extern fn base64_encode(src: [*]const u8, srclen: usize, out: [*]u8, outlen: *usize, flags: c_int) void;
-//     pub extern fn base64_stream_encode_init(state: *State, flags: c_int) void;
-//     pub extern fn base64_stream_encode(state: *State, src: [*]const u8, srclen: usize, out: [*]u8, outlen: *usize) void;
-//     pub extern fn base64_stream_encode_final(state: *State, out: [*]u8, outlen: *usize) void;
-//     pub extern fn base64_decode(src: [*]const u8, srclen: usize, out: [*]u8, outlen: *usize, flags: c_int) c_int;
-//     pub extern fn base64_stream_decode_init(state: *State, flags: c_int) void;
-//     pub extern fn base64_stream_decode(state: *State, src: [*]const u8, srclen: usize, out: [*]u8, outlen: *usize) c_int;
-// };
+pub const b64StreamEncoder = struct {
+    state: clib.struct_base64_state,
+    flag: Flag,
+    const Self = @This();
+
+    pub fn init(flag: Flag) Self {
+        var self = Self{
+            .state = undefined,
+            .flag = flag,
+        };
+
+        clib.base64_stream_encode_init(&self.state, @intFromEnum(flag));
+        return self;
+    }
+
+    pub fn update(self: *Self, s: []const u8, out: []u8) []const u8 {
+        var out_len: usize = undefined;
+        clib.base64_stream_encode(&self.state, s.ptr, s.len, out.ptr, &out_len);
+        return out[0..out_len];
+    }
+
+    pub fn final(self: *Self, out: []u8) []const u8 {
+        var out_len: usize = undefined;
+        clib.base64_stream_encode_final(&self.state, out.ptr, &out_len);
+        return out[0..out_len];
+    }
+};
+
+test "test b64StreamEncoder" {
+    const allocator = testing.allocator;
+    var res = std.ArrayList(u8).init(allocator);
+    defer res.deinit();
+
+    const s = [_][]const u8{ "H", "e", "l", "l", "o", " ", "W", "o", "r", "l", "d" };
+
+    var out: [4]u8 = undefined;
+    var encoder = b64StreamEncoder.init(.default);
+
+    for (s) |c| {
+        const part = encoder.update(c, &out);
+        try res.appendSlice(part);
+    }
+
+    try res.appendSlice(encoder.final(&out));
+
+    try testing.expectEqualStrings("SGVsbG8gV29ybGQ=", res.items);
+}
+
+pub const b64StreamDecoder = struct {
+    state: clib.struct_base64_state,
+    flag: Flag,
+    const Self = @This();
+
+    pub fn init(flag: Flag) Self {
+        var self = Self{
+            .state = undefined,
+            .flag = flag,
+        };
+
+        clib.base64_stream_decode_init(&self.state, @intFromEnum(flag));
+        return self;
+    }
+
+    pub fn update(self: *Self, s: []const u8, out: []u8) []const u8 {
+        var out_len: usize = undefined;
+        // TODO: error
+        _ = clib.base64_stream_decode(&self.state, s.ptr, s.len, out.ptr, &out_len);
+        return out[0..out_len];
+    }
+};
+
+test "test b64StreamDecoder" {
+    const allocator = testing.allocator;
+    var res = std.ArrayList(u8).init(allocator);
+    defer res.deinit();
+
+    const s = [_][]const u8{
+        "S", "G", "V", "s", "b", "G", "8", "g", "V", "2", "9", "y", "b", "G", "Q", "=",
+    };
+
+    var out: [4]u8 = undefined;
+    var decoder = b64StreamDecoder.init(.default);
+
+    for (s) |c| {
+        const part = decoder.update(c, &out);
+        try res.appendSlice(part);
+    }
+
+    try testing.expectEqualStrings("Hello World", res.items);
+}
 
 pub fn b64encode(s: []const u8, out: []u8, flag: Flag) []const u8 {
     var out_len: usize = undefined;
+
     clib.base64_encode(s.ptr, s.len, out.ptr, &out_len, @intFromEnum(flag));
 
     return out[0..out_len];
+}
+
+test "base64 encode" {
+    const s = "Hello World";
+
+    var out: [32]u8 = undefined;
+    const res = b64encode(s, &out, .default);
+
+    try testing.expectEqualStrings("SGVsbG8gV29ybGQ=", res);
 }
 
 pub fn b64decode(s: []const u8, out: []u8, flag: Flag) ![]const u8 {
@@ -61,15 +146,6 @@ pub fn b64decode(s: []const u8, out: []u8, flag: Flag) ![]const u8 {
         return error.InvalidInput;
     }
     unreachable;
-}
-
-test "base64 encode" {
-    const s = "Hello World";
-
-    var out: [32]u8 = undefined;
-    const res = b64encode(s, &out, .default);
-
-    try testing.expectEqualStrings("SGVsbG8gV29ybGQ=", res);
 }
 
 test "base64 decode" {
